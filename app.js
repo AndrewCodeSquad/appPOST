47bd83eea07b8d748e36948ba7a6299044db4351
Andrew O <andrew@codesquad.org>
2019-06-12 16:44:53 -0400

    Initial commit of app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// A demonstration of POST routes
+const express = require('express');
+const app =     express();
+const port = 3000;
+
+
+app.listen(port, () => console.log(`Our POST example app is listening on port ${port}!`))
